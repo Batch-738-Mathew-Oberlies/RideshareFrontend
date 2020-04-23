1e422a3882396896f1a4b6cb90e5613c9bde61cb
Gilbert Turner <grturner@5x5code.com>
2020-04-23 17:24:00 -0400

    e2e testing updates
    Corrected the baseUrl in protactor.conf.js and corrected the landingpage and register tests to be passing in a base form.

--- e2e/test/register.spec.js
+++ e2e/test/register.spec.js
@@ -1,26 +1,40 @@
 
 describe('Sign Up tests', function(){
-    let elSignUpLink = element(by.xpath('//*[@id="login-form"]/p/a/signupmodal/a'));
+    let elSignUpLink = element(by.xpath('/html/body/app-root/app-home-page/div/div/div/button[2]/signupmodal/a'));
     let elRegisterHeader = element(by.xpath('/html/body/modal-container/div/div/div[1]/h4'));
-    let firstNameInput
-    let lastNameInput
-    let usernameInput
-    let emailInput
-    let phoneInput
-    let addressInput
-    let cityInput
-    let stateInput
-    let zipCodeInput
-    let driverRadio
+    let firstNameInput = element(by.xpath('//*[@id="firstname"]'));
+    let lastNameInput = element(by.xpath('//*[@id="lastname"]'));
+    let usernameInput = element(by.xpath('//*[@id="userName"]'));
+    let emailInput = element(by.xpath('//*[@id="email"]'));
+    let phoneInput = element(by.xpath('//*[@id="phoneNumber"]'));
+    let addressInput = element(by.xpath('//*[@id="hAddress"]'));
+    let cityInput = element(by.xpath('/html/body/modal-container/div/div/div[2]/input[7]'));
+    let stateInput = element(by.xpath('//*[@id="hState"]'));
+    let zipCodeInput = element(by.xpath('/html/body/modal-container/div/div/div[2]/input[8]'));
+    let driverRadio = element(by.xpath('//*[@id="driver"]'));
+    let submitButton = element('/html/body/modal-container/div/div/div[2]/div[2]/button');
     
     it('Sign up link on login page opens Sign up modal', function() {
-        browser.get('http://http://54.174.82.153:4200/login');
+        browser.get('http://localhost:4200');
         elSignUpLink.click();
         expect(elRegisterHeader.getText()).toBe('Sign Up');
     });
 
     it ('After submit routes to landing page', function() {
-        browser.get('http://http://54.174.82.153:4200/login');
-        elSignUpLink.click();
+        firstNameInput.sendKeys('bob');
+        lastNameInput.sendKeys('ross');
+        usernameInput.sendKeys('bobross');
+        emailInput.sendKeys('bobross@bobross.com');
+        phoneInput.sendKeys('1234567890');
+        addressInput.sendKeys('123 Fake Street');
+        cityInput.sendKeys('Pottsdam');
+        let state = element.all(by.tagName('option')).get(5);
+        state.click();
+        zipCodeInput.sendKeys('23456');
+        driverRadio.click();
+        submitButton.click();
+        expect(browser.getCurrentUrl()).toBe('http://localhost:4200');
+        // Test is passing for right now. 
+        // However this test will likely need to be refined. 
     });
 });
