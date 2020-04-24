1189afd7bb2611fe3cb698e856c12f8913470e06
Logan <logankilby@gmail.com>
2020-04-24 12:13:55 -0400

    added comments to address model

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -4,8 +4,13 @@
  */
 
 export class Address {
+
+    //street number/name
     street1: string;
+
+    //apartment#, floor, suite, etc
     street2: string;
+
     city: string;
     state: string;
     zip: string;
