f1d1738634a4ac19653dfecf3f8d45c8f7533830
cmochsner <cmochsner@gmail.com>
2020-04-28 12:47:07 -0500

    Co-authored-by: Quinn Bierbaum<quinn.bierbaum@gmail.com> Added schedule and view my rides components with associated service. Updated environment files for tripUri.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,19 +1,37 @@
 /**
- * This is the base URI
+ * This file can be replaced during build by using the `fileReplacements` array.
+ * The list of file replacements can be found in `angular.json`.
+ * `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
  */
-const baseUri = `http://localhost`;
 
-const port = '8080';
-  /**
-   * These are the dev environment variables
+const baseUri = `https://back.rideshare.revatureprojects.com/rideshare`;
+//const baseUri = `http://localhost:8081/rideshare`;
+/**
+ * google map api key
+ */
+const googleKey = '';
+ /**
+   * This is the environment config.
    */
+
 export const environment = {
 
   production: false,
-  environmentName: 'Default Dev Environment',
-  userUri: `${baseUri}:${port}/users/`,
-  loginUri: `${baseUri}:${port}/login/`,
-  batchesUri: `${baseUri}:${port}/batches/`,
-  carUri: `${baseUri}:${port}/cars/`,
-  adminUri: `${baseUri}:${port}/admins/`
+  environmentName: 'Default Environment',
+  userUri: `${baseUri}/users/`,
+  loginUri: `${baseUri}/login/`,
+  batchesUri: `${baseUri}/batches/`,
+  carUri: `${baseUri}/cars/`,
+  adminUri: `${baseUri}/admins/`,
+  tripUri: `${baseUri}/trips/`,
+  googleMapKey: `${googleKey}`,
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
