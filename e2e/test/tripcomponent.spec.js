c1d51a281d7e49ccd1e182fb8a0b9e7ae747f5c0
The Camel <jcfo233@gmail.com>
2020-04-28 12:45:26 -0500

    Added navbar tab

---
+++ e2e/test/tripcomponent.spec.js
@@ -0,0 +1,10 @@
+describe('User logged in trip component tests'), function(){
+     //beginning of elements for the login
+     let elLoginButton = element(by.xpath('/html/body/app-root/app-home-page/div[1]/div/div/button[1]'));
+     let elLoginUsername = element(by.xpath('//*[@id="formGroupExampleInput"]'));
+     let elLoginPassword = element(by.xpath('//*[@id="formGroupExampleInput2"]'));
+     let elLoginSubmit = element(by.xpath('//*[@id="sign-in-btn"]'));
+     let elLoggedInHeader = element(by.xpath('//*[@id="navbarDropdown"]/span'));
+
+     //beg
+}
