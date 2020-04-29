cea1fc72ae9d144658bbd60e20bbd91bb876f8d0
Taiketsu <nathanmejia@ymail.com>
2020-04-29 09:31:39 -0600

    WIP writing test suite for registration

--- e2e/test/register.spec.js
+++ e2e/test/register.spec.js
@@ -1,3 +1,4 @@
+import { By } from "protractor";
 
 describe('Sign Up tests', function(){
     let elSignUpLink = element(by.xpath('/html/body/app-root/app-home-page/div/div/div/button[2]/signupmodal/a'));
@@ -13,7 +14,26 @@ describe('Sign Up tests', function(){
     let zipCodeInput = element(by.xpath('/html/body/modal-container/div/div/div[2]/input[8]'));
     let driverRadio = element(by.xpath('//*[@id="driver"]'));
     let submitButton = element('/html/body/modal-container/div/div/div[2]/div[2]/button');
+    let alert = browser.switchTo().alert();
+    let error = 'Insert error text to expect here';
+    let batch = element(By.id('batch'))
+    let state = element(By.id('state'))
     
+    // Below tests are not working as the login page does not have a sign up link at all, and the way forms works has been
+    // completely changed tests need to be reworked for that functionality in mind.
+
+    // list of tests to develop:
+
+    // open sign-up modal from landing page: Done
+
+    // sign up with a fake address and get a popup
+
+    // Once pop up is there hit cancel to go back to form
+
+    // sign up with a real address but needs more info get a pop-up
+
+    // click yes on this popup to accept changes and create a user
+
     it('Sign up link on login page opens Sign up modal', function() {
         browser.get('http://localhost:4200');
         browser.waitForAngular();
@@ -29,7 +49,7 @@ describe('Sign Up tests', function(){
         phoneInput.sendKeys('1234567890');
         addressInput.sendKeys('123 Fake Street');
         cityInput.sendKeys('Pottsdam');
-        let state = element.all(by.tagName('option')).get(5);
+        
         state.click();
         zipCodeInput.sendKeys('23456');
         //driverRadio.click();
