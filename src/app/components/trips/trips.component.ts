22548c1c575f7f4af99fdda8e4d0166284c91f17
Galo Romero <grome014@fiu.edu>
2020-05-04 17:09:36 -0400

    Added DC as a state in the trip component

--- src/app/components/trips/trips.component.ts
+++ src/app/components/trips/trips.component.ts
@@ -120,7 +120,7 @@ export class CreateTripComponent {
   @Input() trip: Trip = new Trip();
   tripModalForm: FormGroup;
   numberArray: number[] = [];
-  states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS',
+  states = ['AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS',
             'KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY',
             'NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
             'WI','WY'];
