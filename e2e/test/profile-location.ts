eea43b5bbe18ae057dd536d203aa5f4edab23361
Jonathan Conner <jonathan.g.conner@gmail.com>
2020-05-05 12:07:08 -0400

    Added protractor tests for profile-location and profile-membership

--- e2e/test/profile-location.ts
+++ e2e/test/profile-location.ts
@@ -1,105 +1,142 @@
 import { browser, element, by } from "protractor";
 import { ProfileLocation } from "../src/profile-location";
+import { protractor } from 'protractor/built/ptor';
 
 var pl = new ProfileLocation();
 
 describe("Profile Location Components Tests", () => {
-    browser.get("http://localhost:4200/");
-    browser.waitForAngular();
-    element(by.id("login-btn")).click();
-    element(by.id("formGroupExampleInput")).sendKeys("tcurd9");
-    element(by.id("formGroupExampleInput2")).sendKeys("");
-    element(by.id("sign-in-btn")).click();
-    // expect(browser.getCurrentUrl()).toBe("http://localhost:4200/landingPage");
+  browser.get("http://localhost:4200/");
+  browser.waitForAngular();
+  element(by.id("login-btn")).click();
+  element(by.id("formGroupExampleInput")).sendKeys("tcurd9");
+  element(by.id("formGroupExampleInput2")).sendKeys("");
+  element(by.id("sign-in-btn")).click();
+  // expect(browser.getCurrentUrl()).toBe("http://localhost:4200/landingPage");
 
-    it("Test 0: Why wont you work", () => {
-      element(by.id("navbarDropdown")).click();
-      browser.sleep(1000);
-      element(by.id("profileDropdown")).click();
-      expect(browser.getCurrentUrl()).toBe("http://localhost:4200/profile");
-    });
-
-
-    it("Test 1: user cannot submit form without making any changes", () => {
-      expect(pc.submit.isEnabled()).toBe(false);
-    });
-
-    it("Test 2: Profile Membership form is prefilled with information", () => {
-      element(by.id("locationButton")).click();
-      expect(pl.homeStreet.getAttribute("value")).toBeTruthy();
-      expect(pl.homeAddressApt.getAttribute("value")).toBeTruthy();
-      expect(pl.homeCity.getAttribute("value")).toBeTruthy();
-      //expect(pl.homeState.getAttribute("value")).toBeTruthy();
-      expect(pl.homeZipcode.getAttribute("value")).toBeTruthy();
-
-      expect(pl.workStreet.getAttribute("value")).toBeTruthy();
-      expect(pl.workAddressApt.getAttribute("value")).toBeTruthy();
-      expect(pl.workCity.getAttribute("value")).toBeTruthy();
-      //expect(pl.workState.getAttribute("value")).toBeTruthy();
-      expect(pl.workZipcode.getAttribute("value")).toBeTruthy();
-    });
+  it("Test 0: Why wont you work", () => {
+    element(by.id("navbarDropdown")).click();
+    browser.sleep(1000);
+    element(by.id("profileDropdown")).click();
+    expect(browser.getCurrentUrl()).toBe("http://localhost:4200/profile");
+  });
 
 
-    it("Test 3: Invalid form control entries.", ()=>{
+  it("Test 1: user cannot submit form without making any changes", () => {
+    expect(pl.submit.isEnabled()).toBe(false);
+  });
 
-      element(by.id("locationButton")).click();
+  it("Test 2: Profile Membership form is prefilled with information", () => {
+    element(by.id("locationButton")).click();
+    expect(pl.homeStreet.getAttribute("value")).toBeTruthy();
+    expect(pl.homeAddressApt.getAttribute("value")).toBeTruthy();
+    expect(pl.homeCity.getAttribute("value")).toBeTruthy();
+    //expect(pl.homeState.getAttribute("value")).toBeTruthy();
+    expect(pl.homeZipcode.getAttribute("value")).toBeTruthy();
 
-      pl.setHomeStreet("8000 Home Street!!");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.homeStreet.clear();
-      pl.setHomeStreet("8000 Test Street");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.homeStreet.clear();
-
-      pl.setHomeAddressApt("!@#$%!");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.homeAddressApt.clear();
-      pl.setHomeAddressApt("APT 111");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.homeAddressApt.clear();
-
-      pl.setHomeCity("%thisisnotacity");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.homeCity.clear();
-      pl.setHomeCity("Charlottesville");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.homeCity.clear();
+    expect(pl.workStreet.getAttribute("value")).toBeTruthy();
+    expect(pl.workAddressApt.getAttribute("value")).toBeTruthy();
+    expect(pl.workCity.getAttribute("value")).toBeTruthy();
+    //expect(pl.workState.getAttribute("value")).toBeTruthy();
+    expect(pl.workZipcode.getAttribute("value")).toBeTruthy();
+  });
 
 
-      pl.setHomeZipcode("fffff");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.homeZipcode.clear();
-      pl.setHomeZipcode("12345");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.homeZipcode.clear();
+  it("Test 3: Invalid form control entries.", () => {
 
-      pl.setWorkStreet("8000 Work Street!!");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.workStreet.clear();
-      pl.setWorkStreet("8000 Work Test Street");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.workStreet.clear();
+    element(by.id("locationButton")).click();
 
-      pl.setWorkAddressApt("@@@");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.workAddressApt.clear();
-      pl.setWorkAddressApt("APT 222");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.workAddressApt.clear();
+    pl.setHomeStreet("8000 Home Street!!");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.homeStreet.clear();
+    pl.setHomeStreet("8000 Test Street");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.homeStreet.clear();
 
-      pl.setWorkCity("%thisisnotacity");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.workCity.clear();
-      pl.setWorkCity("Charlottesville");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.workCity.clear();
+    pl.setHomeAddressApt("!@#$%!");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.homeAddressApt.clear();
+    pl.setHomeAddressApt("APT 111");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.homeAddressApt.clear();
+
+    pl.setHomeCity("%thisisnotacity");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.homeCity.clear();
+    pl.setHomeCity("Charlottesville");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.homeCity.clear();
 
 
-      pl.setWorkZipcode("fffff");
-      expect(pl.submit.isEnabled()).toBe(false);
-      pl.workZipcode.clear();
-      pl.setWorkZipcode("12345");
-      expect(pl.submit.isEnabled()).toBe(true);
-      pl.workZipcode.clear();
-    });
+    pl.setHomeZipcode("fffff");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.homeZipcode.clear();
+    pl.setHomeZipcode("12345");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.homeZipcode.clear();
+
+    pl.setWorkStreet("8000 Work Street!!");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.workStreet.clear();
+    pl.setWorkStreet("8000 Work Test Street");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.workStreet.clear();
+
+    pl.setWorkAddressApt("@@@");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.workAddressApt.clear();
+    pl.setWorkAddressApt("APT 222");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.workAddressApt.clear();
+
+    pl.setWorkCity("%thisisnotacity");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.workCity.clear();
+    pl.setWorkCity("Charlottesville");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.workCity.clear();
+
+    pl.setWorkZipcode("fffff");
+    expect(pl.submit.isEnabled()).toBe(false);
+    pl.workZipcode.clear();
+    pl.setWorkZipcode("12345");
+    expect(pl.submit.isEnabled()).toBe(true);
+    pl.workZipcode.clear();
+
+    expect(pl.submit.isEnabled()).toBe(false);
+
+  });
+
+
+  it("Test 4: Should get a response from the USPS API which is then confirmed.", () => {
+    // Enable the submit button
+    pl.setHomeAddressApt("APT 208!");
+    pl.homeAddressApt.clear();
+    pl.setHomeAddressApt("APT 208");
+    expect(pl.submit.isEnabled()).toBe(true);
+
+    //Submit the data to API
+    pl.submit.click();
+
+    //Check for the "confirm" modal box
+    browser.wait(protractor.ExpectedConditions.alertIsPresent(), 5000);
+    browser.switchTo().alert().accept();
+
+  });
+
+  it("Test 5: Successful form data submission", ()=>{
+
+    expect(pl.submit.isEnabled()).toBe(true);
+
+    //submit the form data
+    pl.submit.click();
+
+    browser.waitForAngular(); //wait for the http request / response to complete
+
+    // response success message is displayed
+    expect(element(by.id("response-success")).isDisplayed()).toBe(true);
+
+  })
+
+
+
 });
--- e2e/test/profile-membership.ts
+++ e2e/test/profile-membership.ts
@@ -3,7 +3,7 @@ import { profileMembership } from '../src/profile-membership';
 
 var pm = new profileMembership();
 
-describe("profile component tests", () => {
+describe("Profile Membership Component Tests", () => {
   browser.get("http://localhost:4200/");
   browser.waitForAngular();
   element(by.id("login-btn")).click();
@@ -12,14 +12,14 @@ describe("profile component tests", () => {
   element(by.id("sign-in-btn")).click();
   // expect(browser.getCurrentUrl()).toBe("http://localhost:4200/landingPage");
 
-  it("Test 0: Why wont you work", () => {
+  it("Test 0: Navigate to profile page", () => {
     element(by.id("navbarDropdown")).click();
     browser.sleep(1000);
     element(by.id("profileDropdown")).click();
     expect(browser.getCurrentUrl()).toBe("http://localhost:4200/profile");
   })
 
-  it("Test #: Profile Membership form is prefilled with information", () => {
+  it("Test 1: Profile Membership form is prefilled with information", () => {
     element(by.id("membershipButton")).click();
     expect(pm.driver.getAttribute("value")).toBeTruthy();
     expect(pm.active.getAttribute("value")).toBeTruthy();
@@ -27,9 +27,42 @@ describe("profile component tests", () => {
     expect(pm.yes.isEnabled()).toBe(true);
   })
 
-  it("Test : submit button is disabled when values are the same", () => {
+  it("Test 2: Submit button is disabled when values are the same", () => {
     expect(pm.submit.isEnabled()).toBe(false);
   })
+
+
+  it("Test 3: Driver acceptingRides buttons display toggle", ()=>{
+
+    pm.setDriver(false);
+    expect(pm.submit.isEnabled()).toBe(true); //submit button enabled
+    expect(pm.yes.isDisplayed()).toBe(false); // acceptingRides 'yes' button is not displayed
+    expect(pm.no.isDisplayed()).toBe(false); // acceptingRides 'no' button is not displayed
+
+    pm.setDriver(true);
+    expect(pm.submit.isEnabled()).toBe(true); //submit button enabled
+    expect(pm.yes.isDisplayed()).toBe(true); // acceptingRides 'yes' button is displayed
+    expect(pm.no.isDisplayed()).toBe(true); // acceptingRides 'no' button is displayed
+
+  })
+
+  it("Test 4: Successful form data send", ()=>{
+    // toggle a form field to enable the submit button
+    pm.setActive(false);
+    pm.setActive(true);
+    expect(pm.submit.isEnabled()).toBe(true);
+
+    //submit the form data
+    pm.submit.click();
+
+    browser.waitForAngular(); //wait for the http request / response to complete
+
+    // response success message is displayed
+    expect(element(by.id("response-success")).isDisplayed()).toBe(true);
+
+  })
+
+
 
 })
 
