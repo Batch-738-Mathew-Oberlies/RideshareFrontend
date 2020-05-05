51a5f94a64cf519a9fec091a7afde04db7abb20c
Jean Lucas <jean@4ray.co>
2020-05-04 23:48:43 -0400

    Correctness/formatting
    Co-authored-by: Galo Romero <grome014@fiu.edu>

--- e2e/test/tripcomponent.spec.js
+++ e2e/test/tripcomponent.spec.js
@@ -1,11 +1,9 @@
-
-describe('User logged in trip component tests', function(){
-     //beginning of elements for the login
+describe('User logged in trip component tests', function() {
+     // Beginning of elements for the login
      let elLoginButton = element(by.buttonText('Login'));
      let elLoginUsername = element(by.xpath('//*[@id="formGroupExampleInput"]'));
      let elLoginSubmit = element(by.xpath('//*[@id="sign-in-btn"]'));
      let elLoggedInHeader = element(by.xpath('//*[@id="navbarDropdown"]/span'));
-     let elHamburger = element(by.css('[class="navbar-toggler-icon"]'));
      let elSchedule = element(by.css('[routerlink="/schedule"]'));
      let elCreateTripButton = element(by.buttonText('Create a Trip'));
      let elModalName = element(by.css('[formcontrolname="name"]'));
@@ -17,7 +15,6 @@ describe('User logged in trip component tests', function(){
      let elModalZip = element(by.css('[formcontrolname="zip"]'));
      let elCalendar = element(by.className('btn btn-outline-secondary calendar'));
      let elMonth = element(by.css('[title="Select month"]'));
-     let elSelectMonth = element(by.css('[value="12"]'));
      let elYear = element(by.css('[title="Select year"]'));
      let elSelectYear = element(by.css('[value="2028"]'));
      let elDay = element(by.css('.btn-light', '27'));
@@ -26,36 +23,28 @@ describe('User logged in trip component tests', function(){
      let elMeridies = element(by.buttonText('AM'));
      let elSubmit = element(by.buttonText('Submit'));
 
-
      browser.get('http://localhost:4200');
 
-
-     //beg
-     it('User Presses the login button and signs in as the logged in user', function(){
-          // browser.driver.manage().window().setSize(xComp,y);
+     it('User presses the login button and signs in as the logged-in user', function() {
           elLoginButton.click();
-          //These are based off of dummy data currently. Change if desired
           elLoginUsername.sendKeys('gpichmann0');
           elLoginSubmit.click();
-          //change this to reflect succesful login
           expect(elLoggedInHeader.getText()).toBe('Grady Pichmann');
       });
 
-     it('User Presses schedule on navbar and is taken to the schedule page', function(){
-
+     it('User presses schedule on navbar and is taken to the schedule page', function() {
           browser.ignoreSynchronization=true;
-   
+
           elSchedule.click();
 
           browser.driver.sleep(3000);
-          
+
           expect(elCreateTripButton.getText()).toBe('Create a Trip');
      });
 
-     it('User clicks on Create a Trip and creates a trip', function(){
+     it('User clicks on Create a Trip and creates a trip', function() {
+          // browser.ignoreSynchronization=true;
 
-          browser.ignoreSynchronization=true;
-   
           elCreateTripButton.click();
           elModalName.sendKeys('Gilbert in Space 2');
           elModalSeats.sendKeys('2');
