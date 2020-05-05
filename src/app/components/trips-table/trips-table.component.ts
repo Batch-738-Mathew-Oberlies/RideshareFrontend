a5dbc72e34a15abf6fa87238d406b2f13dd581f3
Jean Lucas <jean@4ray.co>
2020-05-04 23:52:17 -0400

    Refactor displaying of Trips
    - Conditionally display Register/Remove button for a Trip if you are not
      the driver
    - Ensure that if a user isn't assigned to any Trips as a rider, we still
      show at least an empty table, so that we don't end up with a blank
      page
    - Refactored code for ease of readability
    
    Co-authored-by: Galo Romero <grome014@fiu.edu>

--- src/app/components/trips-table/trips-table.component.ts
+++ src/app/components/trips-table/trips-table.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Trip } from 'src/app/models/trip';
 import { ScheduleService } from 'src/app/services/schedule-service/schedule.service';
-import { AuthService } from 'src/app/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-trips-table',
@@ -9,39 +8,40 @@ import { AuthService } from 'src/app/services/auth-service/auth.service';
   styleUrls: ['./trips-table.component.css'],
 })
 export class TripsTableComponent implements OnInit {
+  constructor(private scheduleService: ScheduleService) { }
 
-  constructor(private serv: ScheduleService, private auth: AuthService) { }
-
-  @Input('trips') trips: Trip[];
-  @Input('rider-trips') riderTrips: Trip[];
-  @Input('caption') caption: String;
+  @Input() trips: Trip[];
+  @Input() riderTrips: Trip[];
+  @Input() caption: string;
   public id: number;
 
   ngOnInit() {
-    this.id=+sessionStorage.getItem("userid");
+    this.id = parseInt(sessionStorage.getItem('userid'), 10);
   }
 
-  addTrip(t: Trip) {
-    this.serv.addTrips(t,this.id).subscribe();
+  addTrip(trip: Trip) {
+    this.scheduleService.addTrips(trip, this.id).subscribe();
   }
 
-  removeTrip(t: Trip) {
-    this.serv.removeTrips(t,this.id).subscribe();
+  removeTrip(trip: Trip) {
+    this.scheduleService.removeTrips(trip, this.id).subscribe();
   }
-  
+
   refresh() {
     window.location.reload();
   }
-  
-  onTrip(t: Trip): boolean {
-    let isTrip: boolean = false;
 
-    this.riderTrips.forEach(trip => {
-      if(t.tripId === trip.tripId) {
-        isTrip=true;
-      };
-    });
+  onTrip(trip: Trip): boolean {
+    let isOnTrip = false;
 
-    return isTrip;
+    if (this.riderTrips !== null) {
+      this.riderTrips.forEach(riderTrip => {
+        if (trip.tripId === riderTrip.tripId) {
+          isOnTrip = true;
+        }
+      });
+    }
+
+    return isOnTrip;
   }
 }
