34f3a49f8e97deb8663778bc1f067971b378dcf9
Kayla Filio <kfilio97@gmail.com>
2020-05-05 15:13:51 -0400

    Added DC to possible states

--- src/app/components/profile-location/profile-location.component.ts
+++ src/app/components/profile-location/profile-location.component.ts
@@ -14,7 +14,7 @@ export class ProfileLocationComponent implements OnInit {
 
   currentUser;
   
-  states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS',
+  states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS',
             'KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY',
             'NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
             'WI','WY'];
