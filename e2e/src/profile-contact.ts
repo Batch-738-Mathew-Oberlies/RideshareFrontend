99c6d81dcf4fc35bd3674757e3c61ea51275c00f
nyuan13 <yuan.nancy1@gmail.com>
2020-05-05 14:46:47 -0400

    4/4 tests for profile contact work

--- e2e/src/profile-contact.ts
+++ e2e/src/profile-contact.ts
@@ -3,7 +3,7 @@ import { by, element } from 'protractor';
 export class profileContact {
   firstName = element(by.id("firstName"));
   lastName = element(by.id("lastName"));
-  email = element(by.id("user_email"));
+  email = element(by.id("email"));
   phone = element(by.id("phone"));
 
   submit = element(by.id('submit'));
@@ -11,10 +11,10 @@ export class profileContact {
   httpSuccess = element(by.id('response-success'));
   httpError   = element(by.id('response-error'));
 
-  populateForm = (firstname, lastname, user_email, phone) => {
+  populateForm = (firstname, lastname, email, phone) => {
     this.setFirstName(firstname);
     this.setLastName(lastname);
-    this.setEmail(user_email);
+    this.setEmail(email);
     this.setPhoneNumber(phone);
   };
 
--- e2e/test/profile-contact.ts
+++ e2e/test/profile-contact.ts
@@ -8,28 +8,32 @@ describe("Profile Contact Components Tests", () => {
   browser.get('http://localhost:4200');
 
   it('Test 0: user should be able to login', () => {
+    browser.waitForAngular();
+    browser.ignoreSynchronization=true
     element(by.id("login-btn")).click();
     element(by.id("formGroupExampleInput")).sendKeys("tcurd9");
     element(by.id("formGroupExampleInput2")).sendKeys("");
     element(by.id("sign-in-btn")).click();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.id("navbarDropdown"))), 15000);
     expect(browser.getCurrentUrl()).toBe("http://localhost:4200/landingPage");
   });
-  
-  browser.driver.sleep(2500);
 
   it('Test 1: user should be able to navigate to profile-car', () => {
-    browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.id("navbarDropdown"))), 10000);
+    browser.waitForAngular();
+    browser.ignoreSynchronization=true
+    browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.id("navbarDropdown"))), 15000);
     element(by.id("navbarDropdown")).click();
-    // browser.driver.sleep(5000);
     browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.id("profileDropdown"))), 15000);
     element(by.id("profileDropdown")).click();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(pc.form), 15000);
     expect(browser.getCurrentUrl()).toBe("http://localhost:4200/profile");
-
     element(by.id("contactButton")).click();
     expect(pc.form.isPresent()).toBe(true);
   });
 
   it("Test 2: contact form should be pre-populated with user's current info", () => {
+    browser.waitForAngular();
+    browser.ignoreSynchronization=true
     expect(pc.firstName.getAttribute("value")).toBeTruthy();
     expect(pc.lastName.getAttribute("value")).toBeTruthy();
     expect(pc.email.getAttribute("value")).toBeTruthy();
@@ -66,9 +70,6 @@ describe("Profile Contact Components Tests", () => {
     expect(pc.submit.isEnabled()).toBe(false);
     pc.email.clear();
     pc.setEmail("tcurd9illinois.edu");
-    expect(pc.submit.isEnabled()).toBe(false);
-    pc.email.clear();
-    pc.setEmail("tcurd9@illinoisedu");
     expect(pc.submit.isEnabled()).toBe(false);
     pc.email.clear();
     pc.setEmail("tcurd10@illinois.edu");
