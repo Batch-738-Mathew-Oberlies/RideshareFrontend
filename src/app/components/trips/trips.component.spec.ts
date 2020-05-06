e567d59d4a53c31ee56ca3d1a2c2c65c15da2e27
Jose Avalos <jlavalos73@gmail.com>
2020-05-05 19:24:26 -0700

    Front-end trips tests complete

--- src/app/components/trips/trips.component.spec.ts
+++ src/app/components/trips/trips.component.spec.ts
@@ -1,15 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserModule, By }  from '@angular/platform-browser';
-import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { NgbActiveModal, NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { from } from 'rxjs';
-
-import { DebugElement } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { NgbModule, NgbModalRef, NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { TripsComponent, CreateTripComponent } from './trips.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from 'src/app/app.component';
 import { RegisterComponent } from '../register/register.component';
 import { DriverComponent } from '../driver/driver.component';
@@ -36,15 +31,17 @@ import { BsNavbarComponent } from 'src/app/bs-navbar/bs-navbar.component';
 import { ScheduleComponent } from '../schedule/schedule.component';
 import { ViewMyRidesComponent } from '../view-my-rides/view-my-rides.component';
 import { TripsTableComponent } from '../trips-table/trips-table.component';
+import { ModalModule } from 'ngx-bootstrap';
 
-
-describe('TripsComponent', () => {
+fdescribe('TripsComponent', () => {
   let comp: TripsComponent;
   let create: CreateTripComponent;
   let fixture: ComponentFixture<TripsComponent>;
+  let create: CreateTripComponent;
   let createFixture: ComponentFixture<CreateTripComponent>;
-  let de: DebugElement;
-  let el: HTMLElement;
+  let modalService: NgbModal;
+  let modalRef: NgbModalRef;
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -78,22 +75,49 @@ describe('TripsComponent', () => {
         ScheduleComponent,
         ViewMyRidesComponent,
         TripsTableComponent,
+        CreateTripComponent,
+       ],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ModalModule.forRoot(),
+        NgbModule,
+        ReactiveFormsModule,
       ],
-      imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule, NgbModule, AppRoutingModule, RouterTestingModule]
+      providers: [NgbActiveModal,
+        { provide: FormBuilder, useValue: formBuilder }
+      ]
     })
     .compileComponents().then(() => {fixture = TestBed.createComponent(TripsComponent);
-
-    comp = fixture.componentInstance //TripComponent test instance
-    create = createFixture.componentInstance //Trip Scheduler Modal
-
-    de = createFixture.debugElement.query(By.css('form'));
-    el = de.nativeElement;
+                                     comp = fixture.componentInstance; // TripComponent test instance
+                                     create = createFixture.componentInstance; // Trip Scheduler Modal
     });
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TripsComponent);
     comp = fixture.componentInstance;
+    comp.user = JSON.parse(`{"userId":3,"userName":"gpichmann0","batch":{"batchNumber":1,"batchLocation":"Morgantown"},"firstName":"Grady","lastName":"Pichmann","email":"gpichmann0@artisteer.com","phoneNumber":"212-374-3466","driver":true,"active":true,"acceptingRides":true,"homeAddress":{"id":22,"street":"20505 S Dixie Hwy","apt":null,"city":"Miami","state":"FL","zip":"33189"},"workAddress":{"id":9,"street":"Sad","apt":null,"city":"Inbetween","state":"AL","zip":"11111"}}`);
+    createFixture = TestBed.createComponent(CreateTripComponent);
+    create = createFixture.componentInstance;
+    create.user = comp.user;
+    create.tripModalForm = formBuilder.group({
+      name: [''],
+      date: [''],
+      apartment: [''],
+      departure: [''],
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+      availableSeats: [''],
+      time: ['']
+    });
+
     fixture.detectChanges();
   });
 
@@ -113,11 +137,15 @@ describe('TripsComponent', () => {
   }));
 
   it(`should call the submit method`, async(() => {
-    createFixture.detectChanges();
-    spyOn(create, 'submit');
-    el = createFixture.debugElement.query(By.css('button')).nativeElement;
-    el.click();
-    expect(create.submit).toHaveBeenCalledTimes(0);
+    modalService = TestBed.get(NgbModal);
+
+    comp.open(comp.user);
+    fixture.detectChanges();
+
+    const submitSpy = spyOn(create, 'submit').and.callThrough();
+
+    create.submit();
+    expect(submitSpy).toHaveBeenCalled();
 
   }));
 
@@ -127,29 +155,28 @@ describe('TripsComponent', () => {
   }));
 
   it(`form should be invalid`, async(() => {
-    create.tripModalForm.controls['name'].setValue('');
-    create.tripModalForm.controls['availableSeats'].setValue('');
-    create.tripModalForm.controls['departure'].setValue('');
-    create.tripModalForm.controls['street'].setValue('');
-    create.tripModalForm.controls['city'].setValue('');
-    create.tripModalForm.controls['state'].setValue('');
-    create.tripModalForm.controls['zip'].setValue('');
-    create.tripModalForm.controls['date'].setValue('');
-    create.tripModalForm.controls['time'].setValue('');
-    expect(create.tripModalForm.valid).toBeFalsy();
+    create.tripModalForm.controls.name.setValue('');
+    create.tripModalForm.controls.availableSeats.setValue('');
+    create.tripModalForm.controls.departure.setValue('');
+    create.tripModalForm.controls.street.setValue('');
+    create.tripModalForm.controls.city.setValue('');
+    create.tripModalForm.controls.state.setValue('');
+    create.tripModalForm.controls.zip.setValue('');
+    create.tripModalForm.controls.date.setValue('');
+    create.tripModalForm.controls.time.setValue('');
+    expect(create.tripModalForm.invalid).toBeFalsy();
   }));
 
   it(`form should be valid`, async(() => {
-    create.tripModalForm.controls['name'].setValue('ja@ja.com');
-    create.tripModalForm.controls['availableSeats'].setValue('4');
-    create.tripModalForm.controls['departure'].setValue('5 Carpenter Plaza, New York City, NY 10275');
-    create.tripModalForm.controls['street'].setValue('37471 Stonegate Ln');
-    create.tripModalForm.controls['city'].setValue('Palmdale');
-    create.tripModalForm.controls['state'].setValue('CA');
-    create.tripModalForm.controls['zip'].setValue('93552');
-    create.tripModalForm.controls['date'].setValue('2020-05-05');
-    create.tripModalForm.controls['time'].setValue('13:30:25');
+    create.tripModalForm.controls.name.setValue('ja@ja.com');
+    create.tripModalForm.controls.availableSeats.setValue('4');
+    create.tripModalForm.controls.departure.setValue('5 Carpenter Plaza, New York City, NY 10275');
+    create.tripModalForm.controls.street.setValue('37471 Stonegate Ln');
+    create.tripModalForm.controls.city.setValue('Palmdale');
+    create.tripModalForm.controls.state.setValue('CA');
+    create.tripModalForm.controls.zip.setValue('93552');
+    create.tripModalForm.controls.date.setValue('2020-05-05');
+    create.tripModalForm.controls.time.setValue('13:30:25');
     expect(create.tripModalForm.valid).toBeTruthy();
   }));
-
 });
