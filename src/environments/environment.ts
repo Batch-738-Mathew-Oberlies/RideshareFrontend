f33599a98112f04c57053d4c3c489bd664385c2d
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-05-06 07:37:02 -0400

    changed to multiservice backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
  * `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
  */
 
-const baseUri = `https://back.rideshare.revatureprojects.com/rideshare/`;
+const baseUri = `https://dev.5x5code.com/`;
 
 /**
  * Set the port to 8080
